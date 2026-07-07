c11f4bbd93f3aa3881f8f678933d5330659f72fa
AndreMalmsteen <andre.mlmst@gmail.com>
2026-07-07 14:04:22 -0400

    add questão 2, fila e bfs

--- estatisticas.h
+++
@@ -1,7 +0,0 @@
-#ifndef ESTATISTICAS_H
-#define ESTATISTICAS_H
-double tempoAtual(void);
-double soma(double tempos[], int n);
-double media(double tempos[], int n);
-double desvioPadrao(double tempos[], int n, double media);
-#endif
